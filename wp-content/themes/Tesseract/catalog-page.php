6da1c36960a430c7b45af5c08044b1082bdd55a5
root <root@ip-172-31-63-227.ec2.internal>
2015-08-09 22:42:54 +0000

    catalog

--- wp-content/themes/Tesseract/catalog-page.php
+++ wp-content/themes/Tesseract/catalog-page.php
@@ -5,39 +5,12 @@
  * @package Tesseract
  */
 
-get_header(); ?>
+get_header(); ?>	
 
-	<div class="row">
-		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', 'page' );
-					
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; ?>
-
-			<?php tesseract_paging_nav(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-		<?php $containers = new WP_Query(array('post_type' => 'containers'));?>
+<?php
+$category = $_GET['c'];
+$containers = new WP_Query(array('post_type' => 'containers','category' ==> $category));?>
+<h1><?php echo $_GET['c']."(".count($containers).")"; ?></h1>
         <div class="row">
             <?php while ($containers->have_posts()): $containers->the_post(); ?> 
             <div class="col-lg-4">
@@ -48,6 +21,6 @@ get_header(); ?>
             </div>
         <?php endwhile; ?>
         </div>
-		</main>
-	</div><!-- #primary -->
+	
+	
 <?php get_footer(); ?>
