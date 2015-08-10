20f3b1868d92556386712ab75ac2b534ff3c4b76
edenchen123 <171759346@qq.com>
2015-08-10 09:06:03 +0800

    asd

--- wp-content/themes/Tesseract/catalog-page.php
+++ wp-content/themes/Tesseract/catalog-page.php
@@ -11,8 +11,8 @@ get_header(); ?>
 $category = $_GET['c'];
 $args = array('post_type' => 'containers','category' => $category);
 $containers = new WP_Query($args); 
-echo $args[1];
 ?>
+<p>Test <?php echo "category: ".$args[1]; ?></p>
 <h1><?php echo $category."(".count($containers).")"; ?></h1>
         <div class="row">
             <?php while ($containers->have_posts()): $containers->the_post(); ?> 
