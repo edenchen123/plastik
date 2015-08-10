da56b1a78e4fe4f4b8e6403fcb489027b4cafa35
edenchen123 <171759346@qq.com>
2015-08-10 09:32:36 +0800

    catalog by category

--- wp-content/themes/Tesseract/catalog-page.php
+++ wp-content/themes/Tesseract/catalog-page.php
@@ -5,25 +5,24 @@
  * @package Tesseract
  */
 
-get_header(); ?>	
+get_header(); ?>
 
 <?php
 $category = $_GET['c'];
 $args = array('post_type' => 'containers','category' => $category);
-$containers = new WP_Query($args); 
+$containers_query = new WP_query($args);
 ?>
-<p>Test <?php echo "category: ".$args[1]; ?></p>
-<h1><?php echo $category."(".count($containers).")"; ?></h1>
+<h1><?php echo $category."(".$containers_query->found_posts.")"; ?></h1>
         <div class="row">
-            <?php while ($containers->have_posts()): $containers->the_post(); ?> 
+            <?php while ($containers_query->have_posts()): $containers_query->the_post(); ?>
             <div class="col-lg-4">
                 <h4><?php the_title() ?></h4>
                 <div class="thumbnail">
-                	<?php the_post_thumbnail('thumbnail'); ?>
+                        <?php the_post_thumbnail('thumbnail'); ?>
                 </div>
             </div>
         <?php endwhile; ?>
         </div>
-	
-	
+
+
 <?php get_footer(); ?>
