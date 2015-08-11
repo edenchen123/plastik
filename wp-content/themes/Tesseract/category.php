3fe0a2aa8c86080b0762e139e774ac69c40373fc
edenchen123 <171759346@qq.com>
2015-08-11 09:26:03 +0800

    sada

--- wp-content/themes/Tesseract/category.php
+++ wp-content/themes/Tesseract/category.php
@@ -6,13 +6,13 @@
  */
 
 get_header(); ?>
-
 <?php
-$category = $_GET['c'];
-$args = array('post_type' => 'containers','category' => $category);
+$_cat_ = get_query_var('cat');
+$category = get_category ($_cat_);
+$args = array('post_type' => 'containers','category' => $_cat_);
 $containers_query = new WP_query($args);
 ?>
-<h1><?php echo $category."(".$containers_query->found_posts.")"; ?></h1>
+<h1><?php echo $category.title."(".$containers_query->found_posts.")"; ?></h1>
         <div class="row">
             <?php while ($containers_query->have_posts()): $containers_query->the_post(); ?>
             <div class="col-lg-4">
