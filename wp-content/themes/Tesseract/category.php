53a0ee719168f19bb4b8713607a4a1d23b86feb6
edenchen123 <171759346@qq.com>
2015-08-13 09:15:12 +0800

    product

--- wp-content/themes/Tesseract/category.php
+++ wp-content/themes/Tesseract/category.php
@@ -17,10 +17,27 @@ $title = single_cat_title();
 echo $title."(".$containers_query->found_posts.")"; ?></h1>
         <div class="row">
             <?php while ($containers_query->have_posts()): $containers_query->the_post(); ?>
-            <div class="col-lg-4">
-                <h4><?php the_title() ?></h4>
-                <div class="thumbnail">
+            <div class="list-item">
+                <div class="list-item-thumbnail">
+                    <div class="thumb">
                         <?php the_post_thumbnail('thumbnail'); ?>
+                    </div>
+                </div>
+                <div class="list-item-info">
+                    <div class="sku-title">
+                        <h4><a href="#"> <?php the_title() ?></a></h4>
+                    </div>
+                    <div class="sku-model">
+                        <label>Model:</label>MGKL2LL
+                    </div>
+                    <div class="short-description">
+                        <li>9.7" Retina display</li>
+                        <li>Apple iOS 8.1</li>
+                        <li>64GB storage capacity</li>
+                        <li>A8X chip </li>
+                        <li>Wi-Fi</li>
+                        <li>8MP iSight camera</li>
+                    </div>
                 </div>
             </div>
         <?php endwhile; ?>
