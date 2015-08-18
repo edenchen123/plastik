27b3dd6a2b8cb78e97636751b09830e8b9c1d78a
edenchen123 <171759346@qq.com>
2015-08-18 10:42:20 +0800

    11

--- wp-content/themes/Tesseract/catalog-page.php
+++ wp-content/themes/Tesseract/catalog-page.php
@@ -6,23 +6,139 @@
  */
 
 get_header(); ?>
-
-<?php
-$category = $_GET['c'];
-$args = array('post_type' => 'containers','category' => $category);
-$containers_query = new WP_query($args);
-?>
-<h1><?php echo $category."(".$containers_query->found_posts.")"; ?></h1>
-        <div class="row">
-            <?php while ($containers_query->have_posts()): $containers_query->the_post(); ?>
-            <div class="col-lg-4">
-                <h4><?php the_title() ?></h4>
-                <div class="thumbnail">
-                        <?php the_post_thumbnail('thumbnail'); ?>
-                </div>
-            </div>
-        <?php endwhile; ?>
-        </div>
-
-
+<section class="products-list">			
+			<div class="container">
+			<!-- Heading Starts -->
+				<h2 class="product-head">Latest Products</h2>
+			<!-- Heading Ends -->
+			<!-- Products Row Starts -->
+				<div class="row">
+				<!-- Product #1 Starts -->
+					<div class="col-md-3 col-sm-6">
+						<div class="product-col">
+							<div class="image">
+								<img src="uploads/2015/08/pl23_round.png" alt="product" class="img-responsive">
+							</div>
+							<div class="caption">
+								<h4><a href="product.html">Simply Organic Seeds</a></h4>
+								<div class="description">
+									We are so lucky living in such a wonderful time. Our almost unlimited ...
+								</div>
+								<div class="price">
+									<span class="price-new">$199.50</span> 
+									<span class="price-old">$249.50</span>
+								</div>
+								<div class="cart-button button-group">
+									<button type="button" title="" class="btn btn-wishlist" data-original-title="Wishlist">
+										<i class="fa fa-heart"></i>
+									</button>
+									<button type="button" title="" class="btn btn-compare" data-original-title="Compare">
+										<i class="fa fa-bar-chart-o"></i>
+									</button>
+									<button type="button" class="btn btn-cart">
+										Add to cart
+										<i class="fa fa-shopping-cart"></i> 
+									</button>									
+								</div>
+							</div>
+						</div>
+					</div>
+				<!-- Product #1 Ends -->		
+				<!-- Product #1 Starts -->
+					<div class="col-md-3 col-sm-6">
+						<div class="product-col">
+							<div class="image">
+								<img src="uploads/2015/08/pl23_round.png" alt="product" class="img-responsive">
+							</div>
+							<div class="caption">
+								<h4><a href="product.html">Simply Organic Seeds</a></h4>
+								<div class="description">
+									We are so lucky living in such a wonderful time. Our almost unlimited ...
+								</div>
+								<div class="price">
+									<span class="price-new">$199.50</span> 
+									<span class="price-old">$249.50</span>
+								</div>
+								<div class="cart-button button-group">
+									<button type="button" title="" class="btn btn-wishlist" data-original-title="Wishlist">
+										<i class="fa fa-heart"></i>
+									</button>
+									<button type="button" title="" class="btn btn-compare" data-original-title="Compare">
+										<i class="fa fa-bar-chart-o"></i>
+									</button>
+									<button type="button" class="btn btn-cart">
+										Add to cart
+										<i class="fa fa-shopping-cart"></i> 
+									</button>									
+								</div>
+							</div>
+						</div>
+					</div>
+				<!-- Product #1 Ends -->	
+				<!-- Product #1 Starts -->
+					<div class="col-md-3 col-sm-6">
+						<div class="product-col">
+							<div class="image">
+								<img src="uploads/2015/08/pl23_round.png" alt="product" class="img-responsive">
+							</div>
+							<div class="caption">
+								<h4><a href="product.html">Simply Organic Seeds</a></h4>
+								<div class="description">
+									We are so lucky living in such a wonderful time. Our almost unlimited ...
+								</div>
+								<div class="price">
+									<span class="price-new">$199.50</span> 
+									<span class="price-old">$249.50</span>
+								</div>
+								<div class="cart-button button-group">
+									<button type="button" title="" class="btn btn-wishlist" data-original-title="Wishlist">
+										<i class="fa fa-heart"></i>
+									</button>
+									<button type="button" title="" class="btn btn-compare" data-original-title="Compare">
+										<i class="fa fa-bar-chart-o"></i>
+									</button>
+									<button type="button" class="btn btn-cart">
+										Add to cart
+										<i class="fa fa-shopping-cart"></i> 
+									</button>									
+								</div>
+							</div>
+						</div>
+					</div>
+				<!-- Product #1 Ends -->	
+				<!-- Product #1 Starts -->
+					<div class="col-md-3 col-sm-6">
+						<div class="product-col">
+							<div class="image">
+								<img src="uploads/2015/08/pl23_round.png" alt="product" class="img-responsive">
+							</div>
+							<div class="caption">
+								<h4><a href="product.html">Simply Organic Seeds</a></h4>
+								<div class="description">
+									We are so lucky living in such a wonderful time. Our almost unlimited ...
+								</div>
+								<div class="price">
+									<span class="price-new">$199.50</span> 
+									<span class="price-old">$249.50</span>
+								</div>
+								<div class="cart-button button-group">
+									<button type="button" title="" class="btn btn-wishlist" data-original-title="Wishlist">
+										<i class="fa fa-heart"></i>
+									</button>
+									<button type="button" title="" class="btn btn-compare" data-original-title="Compare">
+										<i class="fa fa-bar-chart-o"></i>
+									</button>
+									<button type="button" class="btn btn-cart">
+										Add to cart
+										<i class="fa fa-shopping-cart"></i> 
+									</button>									
+								</div>
+							</div>
+						</div>
+					</div>
+				<!-- Product #1 Ends -->	
+				</div>
+			<!-- Products Row Ends -->
+			</div>
+		</section>
 <?php get_footer(); ?>
