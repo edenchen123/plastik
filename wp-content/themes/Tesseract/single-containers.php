d93cc4fd111145d60a2e32b5b0b0d0baa748775c
edenchen123 <171759346@qq.com>
2015-08-20 11:15:38 +0800

    dsad

--- wp-content/themes/Tesseract/single-containers.php
+++ wp-content/themes/Tesseract/single-containers.php
@@ -5,36 +5,36 @@
  * @package Tesseract
  */
 
-?>
+get_header();?>
 
 	<div class="row">
 		<div class="col-md-6">
 			<div class="row">
-				<img src="http://54.173.177.91/wp-content/uploads/2015/08/pl23_round.png" class="img-responsive">
+				<img src="http://54.173.177.91/wp-content/uploads/2015/08/pl23_round.png" width="498" height="498">
 			</div>
 			<div class="row">
-				<img src="http://54.173.177.91/wp-content/uploads/2015/08/pl23_round.png" class="img-responsive">
-				<img src="http://54.173.177.91/wp-content/uploads/2015/08/pl23_round.png" class="img-responsive">
+				<img src="http://54.173.177.91/wp-content/uploads/2015/08/pl23_round.png" width="95" height="95">
+				<img src="http://54.173.177.91/wp-content/uploads/2015/08/pl23_round.png" width="95" height="95">
 			</div>
 		</div>
 		<div class="col-md-6">
-<div class="list-item-info">
-				                    <div class="sku-title">
-				                        <a href="/containers/<?php the_slug();?>"><?php the_title()?></a>
-				                    </div>
-				                    <div class="sku-model">
-				                        <label>Model:</label>MGKL2LL
-				                    </div>
-				                    <div class="short-description">
-				                        <li>15 oz</li>
-				                        <li>black,white</li>
-				                        <li>20 g</li>
-				                        <li>150</li>
-				                        <li>7x4</li>
-				                        <li>Small/Medium/Large</li>
-				                    </div>
-                				</div>
+			<div class="list-item-info">
+                <div class="sku-title">
+                    <a href="/containers/<?php the_slug();?>"><?php the_title()?></a>
+                </div>
+                <div class="sku-model">
+                    <label>Model:</label>MGKL2LL
+                </div>
+                <div class="short-description">
+                    <li>15 oz</li>
+                    <li>black,white</li>
+                    <li>20 g</li>
+                    <li>150</li>
+                    <li>7x4</li>
+                    <li>Small/Medium/Large</li>
+                </div>
+            </div>
 		</div>
-	<div>	
+	</div>	
 
 <?php get_footer(); ?>
