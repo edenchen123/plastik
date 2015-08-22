2b04941ccf94ded494c1439faa265f4f204cfc0f
edenchen123 <171759346@qq.com>
2015-08-22 09:16:29 +0800

    dsasad

--- wp-content/themes/Tesseract/single-containers.php
+++ wp-content/themes/Tesseract/single-containers.php
@@ -17,17 +17,8 @@ function the_slug($echo=true){
 }
 ?>
 
-	<div class="row">
 		<div class="col-md-7">
-			<div class="row">
-				<!-- <img src="http://54.173.177.91/wp-content/uploads/2015/08/pl23_round.png" width="498" height="498"> -->
-
                <?php if ( function_exists( 'soliloquy' ) ) { soliloquy( '7-round-deep-bowl-container', 'slug' ); }?>
-			</div>
-			<!-- <div class="row">
-				<img src="http://54.173.177.91/wp-content/uploads/2015/08/pl23_round.png" width="95" height="95">
-				<img src="http://54.173.177.91/wp-content/uploads/2015/08/pl23_round.png" width="95" height="95">
-			</div> -->
 		</div>
 		<div class="col-md-5">
 			<div class="list-item-info">
@@ -40,13 +31,12 @@ function the_slug($echo=true){
                 <div class="short-description">
                     <li><?php the_field('volume'); ?>&nbsp;oz</li>
                     <li><?php the_field('available_color'); ?></li>
-                    <li><?php the_field('carton_count'); ?>&nbsp;g</li>
+                    <li><?php the_field('carton_count'); ?></li>
                     <li><?php the_field('weight'); ?> &nbsp;g</li>
                     <!--            <li><?php the_field('model'); ?></li> -->
                     <li><?php the_field('size'); ?></li>
                 </div>
             </div>
 		</div>
-	</div>	
 
 <?php get_footer(); ?>
